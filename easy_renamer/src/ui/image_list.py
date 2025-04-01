9f58d0c94e099b2cbdf826ed4189a4f955ea82bb
marishiten4 <the.beginning.806@gmail.com>
2025-04-01 16:48:26 +0900

    Update image_list.py

--- easy_renamer/src/ui/image_list.py
+++ easy_renamer/src/ui/image_list.py
@@ -1,18 +1,53 @@
-from PyQt5.QtWidgets import QListWidget
+from PyQt5.QtWidgets import QListWidget, QPushButton, QVBoxLayout, QWidget
 
-class ImageList(QListWidget):
+class ImageList(QWidget):
     def __init__(self):
         super().__init__()
-        self.setSelectionMode(QListWidget.MultiSelection)
+        self.layout = QVBoxLayout(self)
+        self.list_widget = QListWidget()
+        self.list_widget.setSelectionMode(QListWidget.MultiSelection)
+        
+        self.prev_button = QPushButton("前へ")
+        self.next_button = QPushButton("次へ")
+        self.layout.addWidget(self.list_widget)
+        self.layout.addWidget(self.prev_button)
+        self.layout.addWidget(self.next_button)
+        
+        self.images = []
+        self.page_size = 20
+        self.current_page = 0
+        
+        self.prev_button.clicked.connect(self.prev_page)
+        self.next_button.clicked.connect(self.next_page)
     
     def load_images(self, folder):
-        self.clear()
+        self.images = []
         import os
         for file in os.listdir(folder):
             if file.lower().endswith(('.png', '.jpg', '.jpeg')):
-                item = QListWidgetItem(file)
-                item.setData(32, os.path.join(folder, file))  # フルパスを保存
-                self.addItem(item)
+                self.images.append(os.path.join(folder, file))
+        self.show_page()
+    
+    def show_page(self):
+        self.list_widget.clear()
+        start = self.current_page * self.page_size
+        end = min(start + self.page_size, len(self.images))
+        for i in range(start, end):
+            item = QListWidgetItem(os.path.basename(self.images[i]))
+            item.setData(32, self.images[i])
+            self.list_widget.addItem(item)
+        self.prev_button.setEnabled(self.current_page > 0)
+        self.next_button.setEnabled(end < len(self.images))
+    
+    def prev_page(self):
+        if self.current_page > 0:
+            self.current_page -= 1
+            self.show_page()
+    
+    def next_page(self):
+        if (self.current_page + 1) * self.page_size < len(self.images):
+            self.current_page += 1
+            self.show_page()
     
     def selected_images(self):
-        return [item.data(32) for item in self.selectedItems()]
+        return [item.data(32) for item in self.list_widget.selectedItems()]
