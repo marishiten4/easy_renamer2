e78d473a5145d1b396d600844f052229f211187d
marishiten4 <the.beginning.806@gmail.com>
2025-04-02 17:14:09 +0900

    Delete easy_renamer directory

--- easy_renamer/src/utils/helpers.py
+++
@@ -1,9 +0,0 @@
-def count_fullwidth_chars(text):
-    """全角文字を1、半角文字を0.5としてカウント"""
-    count = 0
-    for char in text:
-        if ord(char) > 127:  # 全角文字
-            count += 1
-        else:
-            count += 0.5
-    return count
